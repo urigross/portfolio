a78d19aaaf847162a0315a89cf8ef10fc2ca2d01
urigross <urigross@gmail.com>
2021-09-28 19:14:18 +0300

    Projects section added + responsive + owl-carousel added

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-             $('.navbar').addClass("sticky");
-        } else{
-            $('.navbar').removeClass("sticky"); 
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
         }
     });
     // toggle menu/navbar
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
-})
+    /* owl-carousel script*/
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        Responsive: {
+            0: {
+                items: 1,
+                nav: false
+            }
+            ,600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+
+        }
+    });
+});
