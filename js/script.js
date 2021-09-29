333184089aa1f9825a6049db4dbad03e062c44e8
urigross <urigross@gmail.com>
2021-09-29 15:32:44 +0300

    Contact section added + responsive + Fixed js bug in owl-carousel regarding responsiveness

--- js/script.js
+++ js/script.js
@@ -16,22 +16,22 @@ $(document).ready(function () {
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
+        autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
-        Responsive: {
-            0: {
-                items: 1,
-                nav: false
-            }
-            ,600: {
+        responsive: {
+            0:{
                 items: 1,
                 nav: false
             },
-            1000: {
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
                 items: 3,
                 nav: false
             }
-
         }
     });
 });
