0900e80b4bf39dd481d1336f8dd095e349764dc1
urigross <urigross@gmail.com>
2021-10-04 23:01:49 +0300

    Fixed profile image in mobile mode for galaxy s9 view

--- js/script.js
+++ js/script.js
@@ -34,13 +34,13 @@ $(document).ready(function () {
     });
     // typing animation script
     var typed = new Typed(".typing", {
-        strings: ["Full stack developer", "Frontend developer", "Designer", "YouTuber", "Freelancer"],
+        strings: ["Full stack", "Frontend", "Designer", "YouTuber", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     })
     var typed = new Typed(".typing-2", {
-        strings: ["Full stack developer", "Frontend developer", "Designer", "YouTuber", "Freelancer"],
+        strings: ["Full stack", "Frontend", "Designer", "YouTuber", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
